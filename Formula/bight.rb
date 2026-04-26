0be503c78da50ccbb23f9e65e4eb5a579ee1757f
Andrew A <AndrewADev@gmail.com>
2026-04-26 20:26:13 +0200

    Fix version constant
    Fix constant for most recent release and have the task standardize the value it receives.

--- Formula/bight.rb
+++ Formula/bight.rb
@@ -1,7 +1,7 @@
 class Bight < Formula
   desc "Patches .env files automatically on git checkout"
   homepage "https://github.com/AndrewADev/bight"
-  version "v0.0.2"
+  version "0.0.2"
   license "MIT"
 
   on_macos do
