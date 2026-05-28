71214ddbe66b943be0a0848e12dc7bd57c7c38c3
Andrew A <AndrewADev@gmail.com>
2026-05-28 21:23:54 +0200

    Fix version string and update step
    Fix the version string, and hopefully address the root cause (or at least remove some ambiguous aliasing).

--- Formula/bight.rb
+++ Formula/bight.rb
@@ -1,7 +1,7 @@
 class Bight < Formula
   desc "Patches .env files automatically on git checkout"
   homepage "https://github.com/AndrewADev/bight"
-  version "v0.2.0"
+  version "0.2.0"
   license "MIT"
 
   on_macos do
